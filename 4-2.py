bb4fce8d323642a35b0d8be613285139fa081d54
Xanderzoom <alizia8011@gmail.com>
2021-07-19 18:38:22 +0800

    second commit

--- 4-2.py
+++ 4-2.py
@@ -1,4 +1,4 @@
 animals = ('Dolphin', 'Orca', 'Sperm Whale')
 for animal in animals:
   print(f"{animal.title()} is my favourite!.")
-#hello
+  print(f"{animal.title()} is the best pet because they can understand emotions.")
