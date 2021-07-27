151a3f48073944060b3c104e308ec79745961d75
Xanderzoom <alizia8011@gmail.com>
2021-07-27 15:23:16 +0800

    commit commit commit

---
+++ foods.py
@@ -0,0 +1,8 @@
+my_foods = ['pizza', 'falafel', 'carrot cake', 'canoli']
+friend_food = my_foods[:]
+
+print("My favourite foods are:")
+print(my_foods)
+
+print("My friends favourite foods")
+print(friend_food)
