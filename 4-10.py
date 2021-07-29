a518b2f3af8f78405b0457cf53d2dd354a0739c1
Xanderzoom <alizia8011@gmail.com>
2021-07-29 14:44:10 +0800

    commit commmit commit commit commit

---
+++ 4-10.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+List = ['bmw', 'audi', 'toyota', 'subaru', 'Camero']
+print("the first three items on the list are")
+print(List[0:3])
+print("the items in the middle of the list are")
+print(List[1:4])
+print("thelast items of the list are")
+print(List[2:5])
--- motorcycles.py
+++ motorcycles.py
@@ -1,13 +1,14 @@
 motorcycle = ['honda', 'yamaha', 'suzuki', 'ducati']
 
 
-#motorcycle.append('ducati')
-#print(motorcycle)
-#motorcycle.insert(0, 'ducati')
+motorcycle.append('ducati')
+print(motorcycle)
+motorcycle.insert(0, 'ducati')
+print(motorcycle)
 #del motorcycle[0]
 #popped_motorcycle = motorcycle.pop()
 #last_owned = motorcycle.pop(1)
 #print("the last motorcycle I owned was a " + last_owned.title() + ".")
-motorcycle.remove('ducati')
-print(motorcycle)
+#motorcycle.remove('ducati')
+#print(motorcycle)
 #print(popped_motorcycle)
