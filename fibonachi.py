f3f2c30bf29c6af0abec9ca1f8c5e4e9b2ce196a
Xanderzoom <alizia8011@gmail.com>
2021-08-03 14:18:24 +0800

    commit the commit the commit the commit

--- fibonachi.py
+++ fibonachi.py
@@ -1,1 +1,14 @@
+def generate_next_fib(a, b, c):
+  a = b
+  b = c
+  c = a + b
+  return a, b, c
+
+a=0
+b=1
+c=1
+
+while c < 1000000:
+    a,b,c = generate_next_fib(a,b,c)
+    print(b,c,c/b,round(c/b,11))
 
